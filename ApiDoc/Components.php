5048975453f9f9fce11a4134b4096d089bec83d5
sun <alex.sun@auto1.com>
2025-01-27 18:53:05 +0100

    Added license

--- ApiDoc/Components.php
+++ ApiDoc/Components.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the auto1-oss/service-api-handler-bundle.
+ *
+ * (c) AUTO1 Group SE https://www.auto1-group.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace Auto1\ServiceAPIHandlerBundle\ApiDoc;
--- Routing/EndpointLoader.php
+++ Routing/EndpointLoader.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the auto1-oss/service-api-handler-bundle.
+ *
+ * (c) AUTO1 Group SE https://www.auto1-group.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Auto1\ServiceAPIHandlerBundle\Routing;
 
 use Auto1\ServiceAPIComponentsBundle\Exception\Core\ConfigurationException;
